8c533016623af4eb6ed4203a00860a9cd1ac5539
Thomas-ND <tomasdnd1@gmail.com>
2026-02-15 10:29:56 -0300

    Create venda_e_lucro_de_carros.py

---
+++ venda_e_lucro_de_carros.py
@@ -0,0 +1,26 @@
+while True:
+    try:
+        print("Esta é uma calculadora para ver o lucro ou prejuizo em vendas de carros.")
+        ci = True
+        while ci == True:
+            cifrao = input("Primeiramente, digite o cifão da moeda usada: ")
+            if not any(char.isdigit() for char in cifrao):
+                break
+            else:
+                print("Digite apenas letras e simbolos.")
+
+        compra = float(input("Digite o valor da compra do carro: "))
+        investimento = float(input("Digite o valor do investimento no carro: "))
+        venda = float(input("Digite o valor da venda: "))
+
+        valor_total = venda - (compra + investimento)
+        if valor_total > 0:
+            print(f"Você teve um lucro de: {cifrao} {valor_total}")
+            break
+        else:
+            print(f"Você teve um prejuizo de: {cifrao} {valor_total}")
+            break
+    except ValueError:
+        print("Você digitou um valor inválido! Tente novamento do começo.")
+        
+
